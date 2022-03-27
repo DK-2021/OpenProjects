28379068555aeff3010655f5281a2fad4fe933b5
DK-2021 <85843884+DK-2021@users.noreply.github.com>
2022-03-27 08:32:34 -0400

    Added Insertion Sort and Quick Sort

--- SortingDictionary.cs
+++ SortingDictionary.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -112,7 +113,79 @@ namespace PracticeConsole
                 }
             }
         }
+        private static void QuickSort(int[] array, int lowIndex, int highIndex)
+        {
+            if (lowIndex >= highIndex)
+            {
+                return;
+            }
 
-        //QuickSort
+            int pivotIndex = new Random().Next(highIndex - lowIndex) + lowIndex;
+            int pivot = array[pivotIndex];
+            swap(array, pivotIndex, highIndex);
+
+            int leftPointer = partition(array, lowIndex, highIndex, pivot);
+
+            quicksort(array, lowIndex, leftPointer - 1);
+            quicksort(array, leftPointer + 1, highIndex);
+
+            private static int partition(int[] array, int lowIndex, int highIndex, int pivot)
+            {
+                int leftPointer = lowIndex;
+                int rightPointer = highIndex - 1;
+
+                while (leftPointer < rightPointer)
+                {
+
+                    // Walk from the left until we find a number greater than the pivot, or hit the right pointer.
+                    while (array[leftPointer] <= pivot && leftPointer < rightPointer)
+                    {
+                        leftPointer++;
+                    }
+
+                    // Walk from the right until we find a number less than the pivot, or hit the left pointer.
+                    while (array[rightPointer] >= pivot && leftPointer < rightPointer)
+                    {
+                        rightPointer--;
+                    }
+
+                    swap(array, leftPointer, rightPointer);
+                }
+
+                // This is different from what the video has, and fixes an issue where the last value could potentially be out of order. 
+                // Thanks to viewer Wilson Barbosa for suggesting the fix!
+                if (array[leftPointer] > array[highIndex])
+                {
+                    swap(array, leftPointer, highIndex);
+                }
+                else
+                {
+                    leftPointer = highIndex;
+                }
+
+                return leftPointer;
+            }
+            private static void swap(int[] array, int index1, int index2)
+            {
+                int temp = array[index1];
+                array[index1] = array[index2];
+                array[index2] = temp;
+            }
+
+        }
+        private static void InsertionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int currentIndex = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > currentIndex)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = currentIndex;
+            }
+
+        }
     }
-}
